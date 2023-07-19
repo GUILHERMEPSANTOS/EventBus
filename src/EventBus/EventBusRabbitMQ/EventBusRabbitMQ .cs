17fa557dc724c280e799c981f31f071c6d436776
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-07-18 22:02:09 -0300

    feat: criação do publisher rabbitmq

--- src/EventBus/EventBusRabbitMQ/EventBusRabbitMQ .cs
+++ src/EventBus/EventBusRabbitMQ/EventBusRabbitMQ .cs
@@ -1,14 +1,47 @@
+using System.Text;
+using System.Text.Json;
 using EventBus.Abstractions;
 using EventBus.Events;
-
+using RabbitMQ.Client;
 
 namespace EventBusRabbitMQ
 {
     public class EventBusRabbitMQ : IEventBus, IDisposable
     {
+        private readonly IRabbitMQPersistentConnection _persistentConnection;
+        private string _queueName;
+        const string BROKER_NAME = "event_bus";
+
+        public EventBusRabbitMQ(IRabbitMQPersistentConnection persistentConnection, string queueName)
+        {
+            _persistentConnection = persistentConnection;
+            _queueName = queueName;
+        }
+
         public void Publish(IntegrationEvent @event)
         {
+            if (!_persistentConnection.IsConnected)
+            {
+                _persistentConnection.TryConnect();
+            }
 
+            var eventName = @event.GetType().Name;
+
+            var channel = _persistentConnection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: BROKER_NAME, type: "direct");
+
+            var body = JsonSerializer.SerializeToUtf8Bytes(@event, inputType: @event.GetType());
+
+            var properties = channel.CreateBasicProperties();
+
+            properties.DeliveryMode = 2;
+
+            channel.BasicPublish(exchange: BROKER_NAME,
+                                 routingKey: eventName,
+                                 mandatory: false,
+                                 basicProperties: properties,
+                                 body: body);
         }
 
         public void Subscribe<TEvent, TEventHandler>()
@@ -25,3 +58,7 @@ namespace EventBusRabbitMQ
 
     }
 }
+
+// Bind 
+// _queueName
+// exchangeName
